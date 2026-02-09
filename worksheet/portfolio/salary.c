c264535625ded8e746fe5dea50704288055c2cb9
Chenxuan Li <rndm2830@leeds.ac.uk>
2026-02-09 15:18:50 +0000

    2.9

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,36 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Chenxuan Li
+ * ID: 201859038
  */
 
- int main( void ) {
+int main( void ) {
 
-    // define and initialise variables for the problem data 
+   // define and initialise variables for the problem data 
+   float salary = 24000.0;
+   float NI_rate = 0.08;
+   float tax_rate = 0.2;
+   float salary_after_NI, NI_contribution, tax_contribution, take_home_salary;
+   
+   // calculate the deductions and final take-home salary
+   
+   NI_contribution = salary * NI_rate;
+   salary_after_NI = salary - NI_contribution;
+   if (salary_after_NI <= 12500.0) {
+      tax_contribution = 0; 
+   }
+   // If the salary after deducing the national insurance is smaller than or equal to 12500, then the tax contribution is 0.
+   else {
+      tax_contribution = ( salary_after_NI - 12500.0 ) * tax_rate;
+   }
+   take_home_salary = salary - tax_contribution - NI_contribution;
+    
+   // Use only these print statement with appropriate formatting and variable names
+   printf("Salary £%.2f\n",salary);
+   printf("NI contribution £%.2f\n",NI_contribution);
+   printf("Tax contribution £%.2f\n",tax_contribution);
+   printf("Take home salary £%.2f\n",take_home_salary);
 
-    // calculate the deductions and final take-home salary
-
-    // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
-
-    return 0;
- }
+   return 0;
+}
